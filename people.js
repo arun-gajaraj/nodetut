041a16c9a3a999801b4c44354e04e3a95aa2c927
Arun G <arun.gajaraj@qubecinema.com>
2022-12-18 20:11:41 +0530

    init

---
+++ people.js
@@ -0,0 +1,10 @@
+const people = ["Arun", "Melinda", "Bill", "Jeff"]
+const ages = [27, 28, 26, 23]
+
+// console.log(people)
+
+module.exports = {
+    people,
+    ages,
+}
+
---
+++ test.js
@@ -0,0 +1,26 @@
+// const name = 'mario';
+// console.log(name)
+
+
+// const greet = (yourName) => {
+//     console.log(`Hello, ${yourName}`);
+// }
+
+// greet('Arun');
+// greet('Melinda');
+
+// setTimeout(() => {
+//     console.log("in the timeout")
+//     clearInterval(interval)
+// }, 3000)
+
+// const interval = setInterval(() => {
+//     console.log("In the Interval")
+// }, 1000)
+
+console.log(__dirname)
+console.log(__filename)
+
+const os = require('os');
+
+console.log(os.platform(), os.homedir())
